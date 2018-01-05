8daa53c44058c1743cf7272d29f3f62a63add616
Mohammad Khashashneh <moha@localhost.localdomain>
2018-01-05 19:36:21 +0200

    Creating the basic game scene, field, and 2 tanks

---
+++ levels/field.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "field"
+  type: "sprite"
+  data: "tile_set: \"/levels/tanks_atlas.atlas\"\n"
+  "default_animation: \"bg\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
